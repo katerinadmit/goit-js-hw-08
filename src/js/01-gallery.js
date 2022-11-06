c58be92a355acc35e8f4027eada3e49419920c9d
Katerina Dmytrenko <107727106+katerinadmit@users.noreply.github.com>
2022-11-06 18:38:15 +0200

    1+-

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,31 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+console.log(SimpleLightbox)
+const photoContainer  = document.querySelector('.gallery')
+const galleryMarkup  = createGalleryItemsMarkup(galleryItems);
+
+photoContainer .insertAdjacentHTML('beforeend', galleryMarkup );
+
+function createGalleryItemsMarkup (galleryItems){
+return galleryItems.map(({ preview, original, description }) => {
+
+    return `
+        
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `
+   
+    }).join('');
+}
+
+ let lightbox = new SimpleLightbox('.gallery a', {
+        captionsData:'alt',
+        captionSelector:'img',
+        captionDelay:250,
+    });
