82761312fe6fe0ddc1baa42ff184e9c67739bfc3
Katerina Dmytrenko <107727106+katerinadmit@users.noreply.github.com>
2022-11-07 13:28:00 +0200

    3+-

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
 
+let formData = {};
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+unsavedInfo();
+
+function onFormInput(evt) {
+  if (evt.target.value) {
+    formData[evt.target.name] = evt.target.value;
+  }
+  formData[evt.target.name] = evt.target.value;
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+  const formElements = evt.target.elements;
+  if (formElements.email.value === '' || formElements.message.value === '') {
+    return alert('Please check all field!');
+  }
+  evt.currentTarget.reset();
+  console.log(formData);
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+  formData = {};
+}
+
+function unsavedInfo() {
+  let data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+  if (data) {
+    refs.input.value = !!data.email ? data.email : '';
+    refs.textarea.value = !!data.message ? data.message : '';
+    formData = data;
+  }
+}
