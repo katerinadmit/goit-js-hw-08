8fa23333512077b5dd7769231952e9888956d8fa
Katerina Dmytrenko <107727106+katerinadmit@users.noreply.github.com>
2022-11-07 15:47:00 +0200

    +++

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+console.log(Player)
 
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+const onVideoTimeupdate = function (data) {
+ 
+  localStorage.setItem(LOCAL_STORAGE_KEY, data.seconds);
+};
+
+player.on('timeupdate', throttle(onVideoTimeupdate, 1000));
+
+const timeVideoData = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+player
+  .setCurrentTime(timeVideoData)
+  .then(function (seconds) {
+    
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':        
+        break;
+      default:       
+        break;
+    }
+  });
